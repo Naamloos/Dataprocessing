0f605c32950ba2af534cbb3d197b191a6ee49886
Ryan <naamloosdt@gmail.com>
2020-04-06 01:10:09 +0200

    Cleanup/comments/docs

--- Dataprocessing/DataprocessingApi/wwwroot/include/script.js
+++ Dataprocessing/DataprocessingApi/wwwroot/include/script.js
@@ -1,72 +1,321 @@
 ﻿Chart.defaults.global.legend.display = false;
 
-class ContentSchema {
-    constructor(content, schema) {
+// Class used for returning http body / schema header combo.
+class ContentSchema
+{
+    constructor(content, schema)
+    {
         this.content = content;
         this.schema = schema;
     }
 }
 
-// Returns a promise with the results of a request.
-function httpGET(url) {
-    return new Promise((success, fail) => {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", url, true);
-        xmlHttp.setRequestHeader("accept", RequestType)
-        xmlHttp.send(null);
-        var res = null;
-        xmlHttp.onload = function (e) {
-            success(new ContentSchema(xmlHttp.responseText, xmlHttp.getResponseHeader("link")));
-        };
-    });
-}
-
-function getSchema(path) {
-    return new Promise((success, fail) => {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", path, false);
-        xmlHttp.send(null);
-
-        if (RequestType == "application/json") {
-            console.debug(xmlHttp.responseText)
-            success(JSON.parse(xmlHttp.responseText));
-        }
-        else if (RequestType == "application/xml") {
-            success(xmlHttp.responseText);
-        } else {
-            fail();
-        }
-    })
-}
-
+// Input data vars
 var RequestType = "";
 var Region = "US";
 var State;
 var InputDate;
 
+// Output data vars
 var trendingvideos = null;
 var trendingsongs = null;
 var terrorismevents = null;
 
+// Function to start crunching data.
+function start() {
+
+    // Remove start button
+    document.getElementById("startbtn").remove();
+
+    // Disable date picker, region picker and reqtype picker
+    document.getElementById("datepicker").disabled = true;
+    document.getElementById("region").disabled = true;
+    document.getElementById("reqtype").disabled = true;
+
+    // Get the date input and "parse" it
+    InputDate = document.getElementById("datepicker").value.split("-");
+
+    // Get the region input
+    Region = document.getElementById("region").value;
+
+    // Get the state text element
+    State = document.getElementById("state");
+
+    // Indicate the page is working (crunching the numbers)
+    State.innerHTML = "Working...";
+    State.style.color = "Cyan";
+
+    // Getting request type from selector.
+    var select = document.getElementById("reqtype");
+    var requestType = select.options[select.selectedIndex].value;
+
+    // Store request type.
+    RequestType = requestType;
+
+    // Start all download methods.
+    // These start asynchronous downloads so they do not block the UI.
+    DownloadTopSongs();
+    DownloadTopVideos();
+    DownloadTerrorismEvents();
+}
+
+// Returns a promise with the results of a request. The results contain content and a schema path.
+function httpGET(url)
+{
+    // Return a newly created Promise
+    return new Promise((success, fail) =>
+    {
+        // Do the request
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", url, true);
+        xmlHttp.setRequestHeader("accept", RequestType)
+        xmlHttp.send(null);
+
+        var res = null;
+        // Build event handler for onload
+        xmlHttp.onload = function (e)
+        {
+            // Return results
+            success(new ContentSchema(xmlHttp.responseText, xmlHttp.getResponseHeader("link")));
+        };
+    });
+}
+
+// Gets a parsed schema from a path.
+function getSchema(path)
+{
+    return new Promise((success, fail) =>
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", path, false);
+        xmlHttp.send(null);
+
+        if (RequestType == "application/json")
+        {
+            // We can jsut parse the json schema no issues there
+            console.debug(xmlHttp.responseText)
+            success(JSON.parse(xmlHttp.responseText));
+        }
+        else if (RequestType == "application/xml")
+        {
+            // The XML schema gets parsed later.
+            success(xmlHttp.responseText);
+        }
+        else
+        {
+            // uh wat
+            fail();
+        }
+    })
+}
+
+// Downloads the terrorism events from the API
+function DownloadTerrorismEvents()
+{
+    // async request
+    httpGET("/api/Terrorism?day=&year=" + InputDate[0] + "&region=" + Region)
+        .then(function (resp) // success event handler
+        {
+            // validate and parse
+            ValidateAndParse(resp).then((result) => // success event handler
+            {
+                // If not undefined, store result and fill graph
+                if (typeof result !== "undefined")
+                {
+                    terrorismevents = result;
+                    FillTerrorismGraph();
+                }
+                else
+                {
+                    // Give storage var a value to indicate it's done.
+                    terrorismevents = 1;
+                    // Tell user query yielded no results.
+                    document.getElementById("noresults_terr").innerHTML = "Query unfortunately yielded no results.";
+                }
+
+                // Check whether all requests are done.
+                CheckDone();
+            });
+        });
+}
+
+// Downloads the top videos
+function DownloadTopVideos(region)
+{
+    // async get request
+    httpGET("/api/Youtube?day=" + InputDate[2] + "&month=" + InputDate[1] + "&year=" + InputDate[0] + "&region=" + Region)
+        .then(function (resp) // success event handler.
+        {
+            ValidateAndParse(resp).then((result) => // validate and parse, run following on success.
+            {
+                // if not undefined
+                if (typeof result !== "undefined")
+                {
+                    // store result and fill graph
+                    trendingvideos = result;
+                    FillYoutubeGraph();
+                }
+                else
+                {
+                    // Set data on storage var to indicate we're done
+                    trendingvideos = 1;
+                    // Tell user: no results.
+                    document.getElementById("topvideo").innerHTML = "Query unfortunately yielded no results.";
+                }
+                // Check whether we're done.
+                CheckDone();
+            });
+        });
+}
+
+// Downloads spotify top songs
+function DownloadTopSongs(region)
+{
+    // async request
+    httpGET("/api/Spotify?day=" + InputDate[2] + "&month=" + InputDate[1] + "&year=" + InputDate[0] + "&region=" + Region)
+        .then(function (resp)
+        {
+            // validate and parse
+            ValidateAndParse(resp).then((result) =>
+            {
+                // if not undefined
+                if (typeof result !== "undefined")
+                {
+                    // store result and fill graph
+                    trendingsongs = result;
+                    FillSpotifyGraph();
+                }
+                else
+                {
+                    // set a dummy value and tell user there's no results.
+                    trendingsongs = 1;
+                    document.getElementById("topsong").innerHTML = "Query unfortunately yielded no results.";
+                }
+                // Check whether we're done.
+                CheckDone();
+            });
+        });
+}
+
+// validates a response with it's given headers.
+function ValidateAndParse(contentschema)
+{
+    // build a new validation promise
+    return new Promise((success, fail) =>
+    {
+        // JSON validation
+        if (RequestType == "application/json")
+        {
+            // parse content to json
+            var jobject = JSON.parse(contentschema.content);
+
+            // get schema 
+            getSchema(contentschema.schema).then((result) =>
+            {
+                // validate JSON with Ajv
+                var ajv = new Ajv();
+                var valid = ajv.validate(result, jobject);
+
+                // if not valid, fail.
+                if (!valid)
+                {
+                    console.debug("json not valid!!!");
+                    console.debug(ajv.errors);
+                    fail("invalid json");
+                    return;
+                }
+                else
+                {
+                    console.debug("json valid.");
+                }
+
+                // return success.
+                success(jobject);
+            });
+        }
+        // xml validation and parsing
+        else if (RequestType == "application/xml")
+        {
+            // Get schemas
+            getSchema(contentschema.schema).then((result) =>
+            {
+                // validate xml
+                xmlschema(result).validate(contentschema.content).then((validation) =>
+                {
+                    // if not valid
+                    if (!validation.valid)
+                    {
+                        // fail with invalid xml
+                        console.debug("xml not valid!!!");
+                        console.debug(validation.message);
+                        fail("invalid xml");
+                        return;
+                    }
+                    else
+                    {
+                        console.debug("xml valid.");
+                    }
+
+                    // Parse xml to json object to have the exact same objects
+                    var result = xmljson.toJSON(contentschema.content);
+                    result = result[Object.keys(result)[0]];
+                    result = result[Object.keys(result)[1]];// Go down two levels because weird
+                    success(result);
+                });
+            });
+        }
+        else
+        {
+            fail("Invalid request type");
+        }
+    });
+}
+
+// Checks whether all queries are done and notifies the user.
+function CheckDone()
+{
+    if (trendingsongs != null // trending songs store not null
+        && trendingvideos != null // trending videos store not null
+        && terrorismevents != null) // terrorism events store not null
+    {
+        // Change state to done
+        State.innerHTML = "Done! :)";
+        State.style.color = "Green";
+    }
+}
+
+// Fills the spotify graph
 function FillSpotifyGraph() {
-    // Spotify Chart.
+    // Get canvas context
     var ctx = document.getElementById('spotifychart').getContext('2d');
 
+    // Build chart
     var spotifychart = new Chart(ctx, {
         type: 'bar',
-        data: {
+        data:
+        {
             labels: [],
-            datasets: [{
+            datasets:
+            [{
                 label: 'Top songs by streams',
                 data: [],
                 borderWidth: 1
             }]
         },
-        options: {
-            tooltips: {
+        options:
+        {
+            tooltips:
+            {
                 enabled: true
             },
-            onClick: (c, i) => {
+
+
+            // https://stackoverflow.com/questions/37122484/chart-js-bar-chart-click-events
+
+
+            onClick: (c, i) =>
+            {
+                // onclick for song preview :)
                 var e = i[0];
                 console.log(e._index);
                 var label = spotifychart.data.labels[e._index];
@@ -84,6 +333,7 @@ function FillSpotifyGraph() {
         }
     });
 
+    // add all songs with stream count to graph
     trendingsongs.forEach(song => {
         spotifychart.data.labels.push(song.artist + " - " + song.trackname);
         spotifychart.data.datasets.forEach((dataset) => {
@@ -91,11 +341,12 @@ function FillSpotifyGraph() {
         });
     });
 
+    // force update
     spotifychart.update(1000, false);
 }
 
 function FillYoutubeGraph() {
-    // YouTube chart
+    // YouTube chart, same as spotify
     ctx = document.getElementById('youtubechart').getContext('2d');
 
     var youtubechart = new Chart(ctx, {
@@ -139,7 +390,7 @@ function FillYoutubeGraph() {
 }
 
 function FillTerrorismGraph() {
-    // YouTube chart
+    // Terrorism graph, almost same as other two.
     ctx = document.getElementById('terroristchart').getContext('2d');
 
     var terr_graph = new Chart(ctx, {
@@ -189,7 +440,7 @@ function FillTerrorismGraph() {
     })
 
     for (var key in types) {
-        if (key != "null") { // discard null values.
+        if (key != "null" && key != "") { // discard null values.
             console.log(types[key]);
             terr_graph.data.labels.push(key);
             terr_graph.data.datasets.forEach((dataset) => {
@@ -199,146 +450,4 @@ function FillTerrorismGraph() {
     }
 
     terr_graph.update(1000, false);
-}
-
-function CheckDone() {
-    if (trendingsongs != null && trendingvideos != null && terrorismevents != null) {
-        State.innerHTML = "Done! :)";
-        State.style.color = "Green";
-    }
-}
-
-function start() {
-    // Remove start button
-    document.getElementById("startbtn").remove();
-    document.getElementById("datepicker").disabled = true;
-    document.getElementById("region").disabled = true;
-    document.getElementById("reqtype").disabled = true;
-    InputDate = document.getElementById("datepicker").value.split("-");
-    Region = document.getElementById("region").value;
-    console.debug(InputDate);
-    State = document.getElementById("state");
-    State.innerHTML = "Working...";
-    State.style.color = "Cyan";
-
-    // Getting request type
-    var select = document.getElementById("reqtype");
-    var requestType = select.options[select.selectedIndex].value;
-
-    RequestType = requestType;
-
-    // Do downloads
-    DownloadTopSongs("us");
-    DownloadTopVideos("us");
-    DownloadTerrorismEvents("us");
-}
-
-function DownloadTerrorismEvents(region) {
-    httpGET("/api/Terrorism?day=&year=" + InputDate[0] + "&region=" + Region)
-        .then(function (resp) {
-            console.debug(resp.content);
-            console.debug(resp.schema)
-            ValidateAndParse(resp).then((result) => {
-                console.log(result[0]);
-
-                if (typeof result !== "undefined") {
-                    terrorismevents = result;
-                    FillTerrorismGraph();
-                } else {
-                    terrorismevents = 1;
-                    document.getElementById("noresults_terr").innerHTML = "Query unfortunately yielded no results.";
-                }
-
-                CheckDone();
-            });
-        });
-}
-
-function DownloadTopVideos(region) {
-    httpGET("/api/Youtube?day=" + InputDate[2] + "&month=" + InputDate[1] + "&year=" + InputDate[0] + "&region=" + Region)
-        .then(function (resp) {
-            console.debug(resp.content);
-            console.debug(resp.schema)
-            ValidateAndParse(resp).then((result) => {
-
-                if (typeof result !== "undefined") {
-                    trendingvideos = result;
-                    FillYoutubeGraph();
-                } else {
-                    trendingvideos = 1;
-                    document.getElementById("topvideo").innerHTML = "Query unfortunately yielded no results.";
-                }
-                CheckDone();
-            });
-        });
-}
-
-function DownloadTopSongs(region) {
-    httpGET("/api/Spotify?day=" + InputDate[2] + "&month=" + InputDate[1] + "&year=" + InputDate[0] + "&region=" + Region)
-        .then(function (resp) {
-            console.debug(resp.content);
-            console.debug(resp.schema)
-
-            ValidateAndParse(resp).then((result) => {
-
-                if (typeof result !== "undefined") {
-                    trendingsongs = result;
-                    FillSpotifyGraph();
-                } else {
-                    trendingsongs = 1;
-                    document.getElementById("topsong").innerHTML = "Query unfortunately yielded no results.";
-                }
-                CheckDone();
-            });
-        });
-}
-
-function ValidateAndParse(contentschema) {
-    return new Promise((success, fail) => {
-        if (RequestType == "application/json") {
-            console.debug(contentschema.schema);
-            var jobject = JSON.parse(contentschema.content);
-
-            getSchema(contentschema.schema).then((result) => {
-                // validate JSON
-                var ajv = new Ajv();
-                var valid = ajv.validate(result, jobject);
-
-                if (!valid) {
-                    console.debug("json not valid!!!");
-                    console.debug(ajv.errors);
-                    fail("invalid json");
-                }
-                else {
-                    console.debug("json valid.");
-                }
-
-                console.debug(jobject);
-                success(jobject);
-            });
-        }
-        else if (RequestType == "application/xml") {
-            getSchema(contentschema.schema).then((result) => {
-                xmlschema(result).validate(contentschema.content).then((validation) => {
-                    if (!validation.valid) {
-                        console.debug("xml not valid!!!");
-                        console.debug(validation.message);
-                        fail("invalid xml");
-                    }
-                    else {
-                        console.debug("xml valid.");
-                    }
-
-                    var result = xmljson.toJSON(contentschema.content);
-                    result = result[Object.keys(result)[0]];
-                    result = result[Object.keys(result)[1]];// Go down two levels of object bs
-                    console.debug(result);
-                    success(result);
-                });
-            });
-        }
-        else {
-            fail("Invalid request type");
-        }
-    });
 }
